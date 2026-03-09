faa6d6c3c5a61a33cca31d70954655621fc556c2
vickijigsaw <v.sukiasova@gmail.com>
2026-03-09 21:20:04 +0400

    changed into turso

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,28 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const createPrismaClient = () => {
+  if (process.env.NODE_ENV === 'production') {
+    if (!process.env.TURSO_DATABASE_URL) {
+      throw new Error('TURSO_DATABASE_URL is not defined')
+    }
+    
+    const libsql = createClient({
+      url: process.env.TURSO_DATABASE_URL,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+    })
+    const adapter = new PrismaLibSQL(libsql)
+    return new PrismaClient({ adapter })
+  }
+
+  return new PrismaClient()
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
